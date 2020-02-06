0d81548b319aa0d785fcaf32855e6a9d695a0442
Richard Cooke <g00331787@gmit.ie>
2020-02-06 01:51:29 +0000

    Started basic main

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char *argv[])
+{
+	FILE *file;
+
+	if (file == NULL)
+	{
+		printf("Error, could not open file.\n");
+	}
+
+	return 0;
+}
+
